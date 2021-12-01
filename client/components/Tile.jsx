608d175f3f931519739b52afe831aa8a564e1a4a
nathan-zaloum <51972215+nathan-zaloum@users.noreply.github.com>
2021-12-02 12:43:40 +1300

    Added check and styling for items on the grid

--- client/components/Tile.jsx
+++ client/components/Tile.jsx
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from 'react'
 
 const Tile = (props) => {
   const [style, setStyle] = useState ('')
+  const [item, setItem] = useState('')
 
   useEffect(() => {
     props.content.includes('floor') ? setStyle('grey') :
     props.content.includes('wall') ? setStyle('black') :
     props.content.includes('door-in') ? setStyle('green') : setStyle('red')
+
+    props.content.includes('food') ? setItem('green') :
+    props.content.includes('sword') ? setItem('blue') :
+    props.content.includes('key') ? setItem('purple') :
+    props.content.includes('guard') ? setItem('red') : setItem('transparent')
   }, [])
 
   return (
     <div className='tile' id={props.id} style={{ backgroundColor: style }}>
-      {/* <svg viewBox='0 0 110 110'>
-        <use href='assets.svg#tile'></use>
-      </svg> */}
+      <div className='item' style={{backgroundColor: item}}></div>
     </div>
   )
 }
