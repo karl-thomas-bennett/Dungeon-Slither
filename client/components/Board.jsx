27ac8f7e61cc9a153324e8505d0b488cd39dc9bd
Karl Bennett <89493611+karl-thomas-bennett@users.noreply.github.com>
2021-12-02 16:58:27 +1300

    Add moving snake

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import ReactDOM from 'react-dom'
 import KeyboardEventHandler from 'react-keyboard-event-handler'
 import Tile from './Tile'
 
@@ -8,26 +7,65 @@ function Board(props) {
   const tiles = []
   for (let i = 1; i <= boardSize; i++) {
     for (let j = 1; j <= boardSize; j++) {
-      tiles.push(i + ', ' + j)
+      tiles.push(j + ', ' + i)
     }
   }
+
   const [snake, setSnake] = useState([[2, 5], [3, 5], [4, 5], [5, 5]])
-  const handleSnakeDangerously = (key, e) => {
-    console.log(key)
+  const [direction, setDirection] = useState('left')
+  const [lastDirection, setLastDirection] = useState('left')
+  const [toggle, setToggle] = useState(true)
+  const handleKeys = (key, e) => {
     switch (key) {
       case 'w':
-        setSnake([[snake[0][0], snake[0][1] + 1], [snake[0][0], snake[0][1]],
-        [snake[1][0], snake[1][1]], [snake[2][0], snake[2][1]]])
-        break
+        if (lastDirection !== 'down') {
+          setDirection('up')
+        }
+        break;
       case 'd':
-        setSnake([[snake[0][0] + 1, snake[0][1]], [snake[0][0], snake[0][1]],
-        [snake[1][0], snake[1][1]], [snake[2][0], snake[2][1]]])
-        break
+        if (lastDirection !== 'left') {
+          setDirection('right')
+        }
+        break;
       case 's':
+        if (lastDirection !== 'up') {
+          setDirection('down')
+        }
+        break;
+      case 'a':
+        if (lastDirection !== 'right') {
+          setDirection('left')
+        }
+        break;
+    }
+  }
+
+  useEffect(() => {
+    handleSnakeDangerously(direction)
+    setLastDirection(direction)
+  }, [toggle])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setToggle(toggle => !toggle)
+    }, 400)
+    return () => clearInterval(timer)
+  }, [])
+  const handleSnakeDangerously = (direction) => {
+    switch (direction) {
+      case 'up':
         setSnake([[snake[0][0], snake[0][1] - 1], [snake[0][0], snake[0][1]],
         [snake[1][0], snake[1][1]], [snake[2][0], snake[2][1]]])
         break
-      case 'a':
+      case 'right':
+        setSnake([[snake[0][0] + 1, snake[0][1]], [snake[0][0], snake[0][1]],
+        [snake[1][0], snake[1][1]], [snake[2][0], snake[2][1]]])
+        break
+      case 'down':
+        setSnake([[snake[0][0], snake[0][1] + 1], [snake[0][0], snake[0][1]],
+        [snake[1][0], snake[1][1]], [snake[2][0], snake[2][1]]])
+        break
+      case 'left':
         setSnake([[snake[0][0] - 1, snake[0][1]], [snake[0][0], snake[0][1]],
         [snake[1][0], snake[1][1]], [snake[2][0], snake[2][1]]])
         break;
@@ -38,10 +76,12 @@ function Board(props) {
 
   return (
     <div className="board" style={{ gridTemplateColumns: `repeat(${boardSize}, 1fr)` }}>
-      <KeyboardEventHandler handleKeys={['alphabetic']} onKeyEvent={handleSnakeDangerously} />
+      <KeyboardEventHandler handleKeys={['alphabetic']} onKeyEvent={handleKeys} />
       {tiles.map(item => <Tile key={item} id={item} snake={snake} />)}
     </div>
   )
 }
+
+
 
 export default Board
