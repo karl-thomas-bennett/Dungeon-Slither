fff2fa25e4e373919f458ff8acb5dfd9407c1f9d
Karl Bennett <89493611+karl-thomas-bennett@users.noreply.github.com>
2021-12-03 16:39:54 +1300

    Add content to test migration

--- server/db/migrations/20211203163622_test.js
+++ server/db/migrations/20211203163622_test.js
@@ -1,8 +1,9 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('test', table => {
+    table.increments('id').primary()
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable('test')
 };
