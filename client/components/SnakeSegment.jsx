deaecbe7153a436f2b3aa1871abb2c40a92d6780
Karl Bennett <89493611+karl-thomas-bennett@users.noreply.github.com>
2021-12-03 15:40:18 +1300

    Add new snake images and compatibility

--- client/components/SnakeSegment.jsx
+++ client/components/SnakeSegment.jsx
@@ -1,25 +1,37 @@
 import React from 'react'
+import imageDict from '../utils/imageDictionary'
 
 const SnakeSegment = ({ snake, pos }) => {
-
-  const i = snake.indexOf(pos)
+  console.log("Snake:", snake, "Pos:", pos)
+  const i = snake.findIndex(segment => segment[0] === pos[0] && segment[1] === pos[1])
   const segment = getSegment(snake[i - 1], snake[i], snake[i + 1])
+  const image = imageDict.find(image => {
+    return image.cases.find(imageCase => {
+      console.log(imageCase, segment)
+      return imageCase.name === segment
+    })
+  })
+
+  let className = 'snake'
+  if (image.cases.find(imageCase => imageCase.name === segment).flipped) {
+    className += ' flipped'
+  }
+
 
   return (
-    <svg viewBox="0 0 110 110">
-      <use href="assets.svg#head"></use>
-    </svg>
+    <img src={'/segments/' + image.name + '.png'} className={className} />
   )
 }
 
 function getSegment(prev, current, next) {
+  console.log('prev:', prev, 'current:', current, 'next:', next)
   if (prev === undefined) {
     return 'head-' + getDirection(next, current)
   }
   if (next === undefined) {
     return 'tail-' + getDirection(current, prev)
   }
-  return 'body-' + getDirection(current, prev) + getDirection(next, current)
+  return 'body-' + getDirection(current, prev) + '-' + getDirection(next, current)
 }
 
 function getDirection(a, b) {
