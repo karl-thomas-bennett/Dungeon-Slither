9f297db4b6a31baa1645bd5793144cac6b306a65
Karl Bennett <89493611+karl-thomas-bennett@users.noreply.github.com>
2021-12-06 10:52:59 +1300

    Add setGameState action

--- client/actions/game.js
+++ client/actions/game.js
@@ -1,5 +1,13 @@
 
 export const SET_GAME_STATE = 'SET_GAME_STATE'
+export const SET_TILE_CONTENT = 'SET_TILE_CONTENT'
+export const setGameState = (coord, content) => {
+  return {
+    type: SET_TILE_CONTENT,
+    coord,
+    content
+  }
+}
 export const setGameState = (gameState) => {
   return {
     type: SET_GAME_STATE,
--- client/reducers/tiles.js
+++ client/reducers/tiles.js
@@ -1,13 +1,14 @@
 import { prototypeLevel } from '../prototype-data'
+import { SET_TILE_CONTENT } from '../actions/game'
 
 const initialState = prototypeLevel
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TILE':
-      return [ ...state, action.tile ]
-    case 'SET_TILE_CONTENT':
-      return [ ...state, state.find(tile => tile.coord === action.coord).content = action.content ]
+      return [...state, action.tile]
+    case SET_TILE_CONTENT:
+      return [...state, state.find(tile => tile.coord === action.coord).content = action.content]
     case 'RESET':
       return initialState
     default:
