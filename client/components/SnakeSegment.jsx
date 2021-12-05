3292451ebea825621665e40bd475c5460340f9b6
Karl Bennett <89493611+karl-thomas-bennett@users.noreply.github.com>
2021-12-06 09:58:26 +1300

    Remove console logs

--- client/components/SnakeSegment.jsx
+++ client/components/SnakeSegment.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import imageDict from '../utils/imageDictionary'
 
 const SnakeSegment = ({ snake, pos }) => {
-  console.log("Snake:", snake, "Pos:", pos)
   const i = snake.findIndex(segment => segment[0] === pos[0] && segment[1] === pos[1])
   const segment = getSegment(snake[i - 1], snake[i], snake[i + 1])
   const image = imageDict.find(image => {
     return image.cases.find(imageCase => {
-      console.log(imageCase, segment)
       return imageCase.name === segment
     })
   })
@@ -24,7 +22,6 @@ const SnakeSegment = ({ snake, pos }) => {
 }
 
 function getSegment(prev, current, next) {
-  console.log('prev:', prev, 'current:', current, 'next:', next)
   if (prev === undefined) {
     return 'head-' + getDirection(next, current)
   }
