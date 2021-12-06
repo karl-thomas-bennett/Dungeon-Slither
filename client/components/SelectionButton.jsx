7fb565470539c2c4dc928e68b794adb3af55e1a0
nathan-zaloum <51972215+nathan-zaloum@users.noreply.github.com>
2021-12-07 10:34:07 +1300

    Editor logic and legend styling

--- client/components/SelectionButton.jsx
+++ client/components/SelectionButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { setSelection } from '../actions/level-data'
@@ -6,12 +6,25 @@ import { setSelection } from '../actions/level-data'
 const SelectionButton = (props) => {
   const selection = useSelector(store => store.levelData.selection)
   const dispatch  = useDispatch()
+  const [legend, setLegend] = useState('')
 
   const selectionHandler = (selection) => { dispatch(setSelection(selection)) }
 
+  useEffect(() => {
+    if (props.value === 'floor')    { setLegend('grey')   }
+    if (props.value === 'wall')     { setLegend('black')  }
+    if (props.value === 'door-in')  { setLegend('green')  }
+    if (props.value === 'door-out') { setLegend('orange') }
+    if (props.value === 'food')     { setLegend('yellow') }
+    if (props.value === 'key')      { setLegend('blue')   }
+    if (props.value === 'sword')    { setLegend('purple') }
+    if (props.value === 'guard')    { setLegend('red')    }
+    if (props.value === 'empty')    { setLegend('')       }
+  }, [])
+
   return (
     <button className='selection-button' disabled={selection === props.value} onClick={() => selectionHandler(props.value)}>{props.name}
-      <div className='selection-legend'></div>
+      <div className='selection-legend' style={{backgroundColor: legend}}></div>
     </button>
   )
 }
--- client/components/TileDesign.jsx
+++ client/components/TileDesign.jsx
@@ -11,21 +11,22 @@ const TileDesign = (props) => {
 
   const [terrainColor, setTerrainColor] = useState ('')
   const [itemColor, setItemColor] = useState('')
+  const [upper, setUpper] = useState('')
   const coord = props.id
-  const upper = String(Math.sqrt(levelMakerState.length) - 1)
 
   const removeCheck  = ['remove', 'wall', 'door-in', 'door-out']
   const cornerCheck = ['0,0', '0,1', '0,2', '1,0', '2,0', `0,${upper}`, `0,${upper - 1}`, `0,${upper - 2}`, `1,${upper}`, `2,${upper}`, `${upper},0`, `${upper},1`, `${upper},2`, `${upper - 1},0`, `${upper - 2},0`, `${upper},${upper}`, `${upper},${upper - 1}`, `${upper},${upper - 2}`, `${upper - 1},${upper}`, `${upper - 2},${upper}`]
+  // const cornerCheck = ['0,0', '0,1', '0,2', '1,0', '2,0', '0,19', '0,18', '0,17', '1,19', '2,19', '19,0', '19,1', '19,2', '18,0', '17,0', '19,19', '19,18', '19,17', '18,19', '17,19']
 
-  const checks = () => {
+  const stylingChecks = () => {
     let content = levelMakerState.find(tile => tile.coord === coord).content
-    // Terrain checks
-    if (content.includes('floor'))    { setTerrainColor('grey')  }
-    if (content.includes('wall'))     { setTerrainColor('black') }
-    if (content.includes('door-in'))  { setTerrainColor('green') }
-    if (content.includes('door-out')) { setTerrainColor('red')   }
-    // Item checks
-    if (content.includes('food'))  { setItemColor('green')  }
+    // Terrain styling
+    if (content.includes('floor'))    { setTerrainColor('grey')   }
+    if (content.includes('wall'))     { setTerrainColor('black')  }
+    if (content.includes('door-in'))  { setTerrainColor('green')  }
+    if (content.includes('door-out')) { setTerrainColor('orange') }
+    // Item styling
+    if (content.includes('food'))  { setItemColor('yellow') }
     if (content.includes('key'))   { setItemColor('blue')   }
     if (content.includes('sword')) { setItemColor('purple') }
     if (content.includes('guard')) { setItemColor('red')    }
@@ -33,56 +34,64 @@ const TileDesign = (props) => {
   }
 
   useEffect(() => {
-    checks()
+    stylingChecks()
   }, [levelMakerState.find(tile => tile.coord === coord).content])
+
+  useEffect(() => {
+    setUpper(String(Math.sqrt(levelMakerState.length) - 1))
+  }, [])
 
   const clickHandler = () => {
     const val = levelDataState.selection
     const bounds = coord.split(',')
     if (bounds.includes('0') || bounds.includes(upper)) {
+      
       if (val === 'door-in') {
-        if (levelDataState.doorIn === 0) {
-          checkDoorIn(val)
-        }
-        if (levelDataState.doorIn === 1) {
-          if (cornerCheck.includes(coord)) {
+        if (!cornerCheck.includes(coord)) {
+          if (levelDataState.doorIn === 0) {
+            checkDoorIn(val)
+          }
+          if (levelDataState.doorIn === 1) {
             if (checkNeigbors('door-in')) {
               checkDoorIn(val)
             } else {
               alert('Please place the second door piece next to the first')
             }
-          } else {
-            alert('You cannot place doors too close to the corner')
           }
-        }
-        if (levelDataState.doorIn === 2) {
-          alert('You have already placed an entrance')
+          if (levelDataState.doorIn === 2) {
+            alert('You have already placed an entrance')
+          }
+        } else {
+          alert('You cannot place doors too close to the corner')
         }
       }
+
       if (val === 'door-out') {
-        if (levelDataState.doorOut === 0) {
-          checkDoorOut(val)
-        }
-        if (levelDataState.doorOut === 1) {
-          if (cornerCheck.includes(coord)) {
+        if (!cornerCheck.includes(coord)) {
+          if (levelDataState.doorOut === 0) {
+            checkDoorOut(val)
+          }
+          if (levelDataState.doorOut === 1) {
             if (checkNeigbors('door-out')) {
               checkDoorOut(val)
             } else {
               alert('Please place the second door piece next to the first')
             }
-          } else {
-            alert('You cannot place doors too close to the corner')
           }
-        }
-        if (levelDataState.doorOut === 2) {
-          alert('You have already placed an exit')
+          if (levelDataState.doorOut === 2) {
+            alert('You have already placed an exit')
+          }
+        } else {
+          alert('You cannot place doors too close to the corner')
         }
       }
-      if (val ===  'wall') { dispatch(setTerrainType(coord, val)) }
+
+      if (val === 'wall') { dispatch(setTerrainType(coord, val)) }
       if (val === 'floor') { alert('You cannot place floor tiles along the map edge') }
+
     } else {
       if (val === 'floor' || val === 'wall') { dispatch(setTerrainType(coord, val)) }
-      if (val === 'wall') { dispatch(removeItemFromTile(coord)) }
+      if (val === 'wall') { removeItem() }
       if (terrainColor === 'grey') {
         if (val === 'guard') {
           if (levelDataState.guard === false) {
@@ -111,11 +120,25 @@ const TileDesign = (props) => {
         if (val === 'food') {
           dispatch(addItemToTile(coord, val))
         }
+      } else {
+        alert('You must place items on a floor tile')
       }
     }
-    if (removeCheck.includes(val))  { dispatch(removeItemFromTile(coord))  }
+    if (removeCheck.includes(val))  {
+      removeItem()
+    }
   }
 
+  // Check the item in the tile. If it is a limited item then reset it's limit. Remove the item from the tile.
+  const removeItem = () => {
+    const item = levelMakerState.find(tile => tile.coord === coord).content[1]
+    if (item === 'guard') { dispatch(setGuard(false)) }
+    if (item === 'key')   { dispatch(setKey(false))   }
+    if (item === 'sword') { dispatch(setSword(false)) }
+    dispatch(removeItemFromTile(coord))
+  }
+
+  // Check if the entrance is too close to the corner pieces.
   const checkDoorIn = (val) => {
     if (cornerCheck.includes(coord)) {
       alert('You cannot place doors too close to the corner')
@@ -125,6 +148,7 @@ const TileDesign = (props) => {
     }
   }
 
+  // Check if the exit is too close to the corner pieces.
   const checkDoorOut = (val) => {
     if (cornerCheck.includes(coord)) {
       alert('You cannot place doors too close to the corner')
@@ -134,8 +158,8 @@ const TileDesign = (props) => {
     }
   }
 
+  // Check if there is another of the same type next to this piece.
   const checkNeigbors = (input) => {
-    const upper = String(Math.sqrt(levelMakerState.length) - 1)
     const coordX = Number(coord.split(',')[1])
     const coordY = Number(coord.split(',')[0])
     const left  = `${coordY},${coordX - 1}`
@@ -143,12 +167,12 @@ const TileDesign = (props) => {
     const up    = `${coordY - 1},${coordX}`
     const down  = `${coordY + 1},${coordX}`
 
-    if (coordX === 0 || coordX === upper) {
+    if (coordX === 0 || coordX === Number(upper)) {
       if (levelMakerState.find(tile => tile.coord === up).content.includes(input) ||
           levelMakerState.find(tile => tile.coord === down).content.includes(input) )
       { return true } else { return false }
     }
-    if (coordY === 0 || coordY === upper) {
+    if (coordY === 0 || coordY === Number(upper)) {
       if (levelMakerState.find(tile => tile.coord === left).content.includes(input) ||
           levelMakerState.find(tile => tile.coord === right).content.includes(input) )
       { return true } else { return false }
