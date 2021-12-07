11d20e9ecd11e88b7b3751a8e93b273b463bc686
Karl Bennett <89493611+karl-thomas-bennett@users.noreply.github.com>
2021-12-07 16:25:13 +1300

    Add imageList and snake util files

--- client/components/Game/SnakeSegment.jsx
+++ client/components/Game/SnakeSegment.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import imageDict from '../../utils/imageDictionary'
+import { getSegment } from '../../utils/snakeUtil'
 
 const SnakeSegment = ({ snake, pos }) => {
   const i = snake.findIndex(segment => segment[0] === pos[0] && segment[1] === pos[1])
@@ -14,24 +15,6 @@ const SnakeSegment = ({ snake, pos }) => {
   return (
     <img src={'/segments/' + image.name + '.png'} className={className} />
   )
-}
-
-function getSegment(prev, current, next) {
-  if (prev === undefined) {
-    return 'head-' + getDirection(next, current)
-  }
-  if (next === undefined) {
-    return 'tail-' + getDirection(current, prev)
-  }
-  return 'body-' + getDirection(next, current) + '-' + getDirection(current, prev)
-}
-
-function getDirection(a, b) {
-  if (a[0] === b[0]) {
-    return a[1] < b[1] ? 'right' : 'left'
-  } else {
-    return a[0] < b[0] ? 'down' : 'up'
-  }
 }
 
 export default SnakeSegment
