b0ecd84dae3ba14f9011c60e100810ac475c6cb8
nathan-zaloum <51972215+nathan-zaloum@users.noreply.github.com>
2021-12-08 12:51:06 +1300

    Styling for game menus

--- client/components/Game/LevelEditor.jsx
+++ client/components/Game/LevelEditor.jsx
@@ -17,7 +17,7 @@ const LevelEditor = (props) => {
 
   const changeHandle  = (e) => { setNameData(e.target.value) } 
   const resetHandler  = ()  => { dispatch(resetLevelEditor()) }
-  const menuHandle    = ()  => { dispatch(resetLevelEditor()); props.history.push('/rules') }
+  const menuHandle    = ()  => { dispatch(resetLevelEditor()); props.history.push('/') }
   const rulesHandle   = ()  => { props.history.push('/editor/rules') }
   const submitHandler = ()  => { if (nameData.length > 0) {
     const level = {
