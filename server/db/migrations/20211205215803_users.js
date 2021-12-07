7a35ad601c04a7f3138eedd322c117b8b5c43a2d
Karl Bennett <89493611+karl-thomas-bennett@users.noreply.github.com>
2021-12-07 15:31:36 +1300

    Fix migration order

--- server/db/migrations/20211205215803_users.js
+++ server/db/migrations/20211205215803_users.js
@@ -1,16 +1,15 @@
-const e = require("express");
 
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('users', table => {
-    table.increments( 'id' )
-    table.string ( 'firstName' )
-    table.string ( 'lastName' )
-    table.string( 'email' )
-    table.string( 'hash' )
-    table.string( 'roll' )
+    table.increments('id')
+    table.string('firstName')
+    table.string('lastName')
+    table.string('email')
+    table.string('hash')
+    table.string('roll')
   })
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable('users')
 };
